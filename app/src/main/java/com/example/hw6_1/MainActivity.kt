2554fad3dd250c70abe6a84a7a2b36629b4f1ca2
razie rezaee <rezaie127@yahoo.com>
2022-02-15 01:40:01 +0330

    set shared preferences & visibility

--- app/src/main/java/com/example/hw6_1/MainActivity.kt
+++ app/src/main/java/com/example/hw6_1/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.hw6_1
 
+import android.content.Context
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.hw6_1.databinding.ActivityMainBinding
@@ -15,12 +17,18 @@ class MainActivity : AppCompatActivity() {
     var email=""
     var password=""
     var gender=""
+    lateinit var edit:SharedPreferences.Editor
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+        gone()
+
+        pref=getPreferences(Context.MODE_PRIVATE)
+
+
         binding.register.setOnClickListener {
 
             if (binding.editTextFullName.text.isBlank()||binding.editTextUserName.text.isBlank()
@@ -44,19 +52,52 @@ class MainActivity : AppCompatActivity() {
                 userName=binding.editTextUserName.text.toString()
                 email=binding.editTextEmailAddress.text.toString()
                 password=binding.editTextPassword.text.toString()
+
+
+                edit=pref.edit()
+                edit.putString("FullName",fullName)
+                edit.putString("UserName",userName)
+                edit.putString("Email",email)
+                edit.putString("Password",password)
+                edit.putString("Gender",gender)
+                edit.apply()
+                Toast.makeText(this,"ذخیره اطلاعات انجام شد" , Toast.LENGTH_LONG).show()
             }
 
 
         }
 
         binding.showInfo.setOnClickListener {
-            binding.textViewFullName.text=fullName
-            binding.textViewUserName.text=userName
-            binding.textViewEmail.text=email
-            binding.textViewPassword.text=password
-            binding.textViewGender.text=gender
+            binding.textViewFullName.text=pref.getString("FullName",null)
+            binding.textViewUserName.text=pref.getString("UserName","")
+            binding.textViewEmail.text=pref.getString("Email","")
+            binding.textViewPassword.text=pref.getString("Password","")
+            binding.textViewGender.text=pref.getString("Gender","")
+            visible()
         }
 
+        binding.buttonHideInfo.setOnClickListener {
+            gone()
+        }
+
+    }
+
+    private fun visible() {
+        binding.textViewFullName.visibility= View.VISIBLE
+        binding.textViewUserName.visibility= View.VISIBLE
+        binding.textViewEmail.visibility= View.VISIBLE
+        binding.textViewPassword.visibility=View.VISIBLE
+        binding.textViewGender.visibility=View.VISIBLE
+        binding.buttonHideInfo.visibility= View.VISIBLE
+    }
+
+    private fun gone() {
+        binding.textViewFullName.visibility= View.GONE
+        binding.textViewUserName.visibility= View.GONE
+        binding.textViewEmail.visibility= View.GONE
+        binding.textViewPassword.visibility=View.GONE
+        binding.textViewGender.visibility=View.GONE
+        binding.buttonHideInfo.visibility= View.GONE
     }
 
 }
